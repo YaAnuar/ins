8fea657608474adbe6295532fb9ca4c56c2b7780
YaAnuar <42729761+YaAnuar@users.noreply.github.com>
2022-07-14 21:38:34 +0600

    Update client.py

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -13,14 +13,9 @@ async def write_json_to_file(key, url):
 
 
 async def main():
-	await write_json_to_file('w','http://samples.openweathermap.org/data/2.5/forecast?id=2643743&appid=b1b15e88fa797225412429c1c50c122a1')
-	await write_json_to_file('a','http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
-	await write_json_to_file('a','http://samples.openweathermap.org/data/2.5/forecast?id=1526384&appid=b1b15e88fa797225412429c1c50c122a11')
-	# # correct way. -- my api key doesn't fit
-	#await write_json_to_file('w','https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=d9d3c1fda92985791ef7d4d5b9fe5d88')
-	#await write_json_to_file('a','https://api.openweathermap.org/data/3.0/onecall?lat=55.75&lon=37.61&exclude=hourly,daily&appid=d9d3c1fda92985791ef7d4d5b9fe5d88')
-	#await write_json_to_file('a','https://api.openweathermap.org/data/3.0/onecall?lat=43.23&lon=76.88&exclude=hourly,daily&appid=d9d3c1fda92985791ef7d4d5b9fe5d88')
-
+	await write_json_to_file('w','http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d9d3c1fda92985791ef7d4d5b9fe5d88')
+	await write_json_to_file('a','http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=d9d3c1fda92985791ef7d4d5b9fe5d88')
+	await write_json_to_file('a','http://api.openweathermap.org/data/2.5/weather?q=Almaty,kz&APPID=d9d3c1fda92985791ef7d4d5b9fe5d88')
 
 
 loop = asyncio.get_event_loop()
