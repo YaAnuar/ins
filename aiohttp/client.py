a11ed4cd4be374fd1e6529def93906cb0cffda2f
YaAnuar <42729761+YaAnuar@users.noreply.github.com>
2022-07-14 16:29:55 +0600

    Update client.py

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -2,19 +2,17 @@ import aiohttp
 import asyncio
 import json
 
-async def main():
+async def write_json_to_file(url):
     async with aiohttp.ClientSession() as session:
-    	async with session.get('http://samples.openweathermap.org/data/2.5/forecast?id=2643743&appid=b1b15e88fa797225412429c1c50c122a1') as response:
+    	async with session.get(url) as response:
             with open("London.txt", "w") as london:
-            	london.write(str(await response.json()))
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1') as response:
-            with open("Moscow.txt", "w") as Moscow:
-            	Moscow.write(str(await response.json()))
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://samples.openweathermap.org/data/2.5/forecast?id=1526384&appid=b1b15e88fa797225412429c1c50c122a1') as response:
-            with open("Almaty.txt", "w") as Almaty:
-            	Almaty.write(str(await response.json()))
+            	london.write(str(await response.json()))	
+
+
+async def main():
+	await write_json_to_file('http://samples.openweathermap.org/data/2.5/forecast?id=2643743&appid=b1b15e88fa797225412429c1c50c122a1')
+	await write_json_to_file('http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
+	await write_json_to_file('http://samples.openweathermap.org/data/2.5/forecast?id=1526384&appid=b1b15e88fa797225412429c1c50c122a1')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
